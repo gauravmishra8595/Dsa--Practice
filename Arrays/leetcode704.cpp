acd9b510b5078ac8e0915f7c87215caef0ac7ca9
gauravmishra8595 <gauravmishrag999@gmail.com>
2026-02-17 21:21:19 +0530

    solve 704 with for loop

--- Arrays/leetcode704.cpp
+++ Arrays/leetcode704.cpp
@@ -20,6 +20,29 @@ int binarySearch(int arr[], int size, int key){
     return -1;
 }
 
+
+
+// class Solution {
+// public:
+//     int search(vector<int>& nums, int target) {
+//         int start = 0;
+//         int end = nums.size() - 1;
+
+//         for (; start <= end; ) {
+//             int mid = start + (end - start) / 2;
+
+//             if (nums[mid] == target)
+//                 return mid;
+
+//             if (target > nums[mid])
+//                 start = mid + 1;
+//             else
+//                 end = mid - 1;
+//         }
+
+//         return -1;
+//     }
+// };
 int main(){
     int even[10]={2,4,6,8,10,12,14,16,18,20};
     int odd[5]={3,6,9,12,15};
