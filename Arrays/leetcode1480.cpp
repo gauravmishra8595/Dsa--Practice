1f92dba235ead29bd4c597b6fe7bae745515a9eb
gauravmishra8595 <gauravmishrag999@gmail.com>
2026-02-19 21:55:30 +0530

    adding another type solution of 1480

--- Arrays/leetcode1480.cpp
+++ Arrays/leetcode1480.cpp
@@ -11,3 +11,18 @@ using namespace std;
         return nums;
     }
  };
+
+//  class Solution {
+// public:
+//     vector<int> runningSum(vector<int>& nums) {
+//         vector<int> result;
+//         int sum = 0;
+        
+//         for (int i = 0; i < nums.size(); i++) {
+//             sum += nums[i];
+//             result.push_back(sum);
+//         }
+        
+//         return result;
+//     }
+// };
