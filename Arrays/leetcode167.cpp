d6eedb8e3cd1a92894bb12f24bb6277ac9ea5bf7
gauravmishra8595 <gauravmishrag999@gmail.com>
2026-04-03 20:56:02 +0530

    adding 167 leetcode question two pointer approach

---
+++ Arrays/leetcode167.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> twoSum(vector<int>& numbers, int target) {
+    int left = 0;
+    int right = numbers.size() - 1;
+
+    while (left < right) {
+        int sum = numbers[left] + numbers[right];
+
+        if (sum == target) {
+            return {left + 1, right + 1}; // 1-based index
+        } else if (sum < target) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return {};
+}
+
+int main() {
+    vector<int> numbers = {2, 7, 11, 15};
+    int target = 26;
+
+    vector<int> result = twoSum(numbers, target);
+
+    if (!result.empty()) {
+        cout << "Indices: " << result[0] << " " << result[1] << endl;
+    } else {
+        cout << "No solution found" << endl;
+    }
+
+    return 0;
+}
