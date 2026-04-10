4198699abdc0e10eb865f3109f2b241991a39d5d
gauravmishra8595 <gauravmishrag999@gmail.com>
2026-04-10 17:15:37 +0530

    leetcode 27 solution add

---
+++ Arrays/leetcode27.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] != val){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+};
+
+int main() {
+    Solution obj;
+    
+    vector<int> nums = {3, 2, 2, 3};
+    int val = 3;
+
+    int k = obj.removeElement(nums, val);
+
+    cout << "New length: " << k << endl;
+    cout << "Updated array: ";
+    for(int i = 0; i < k; i++) {
+        cout << nums[i] << " ";
+    }
+
+    return 0;
+}
