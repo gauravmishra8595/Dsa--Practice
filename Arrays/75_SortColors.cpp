f5df7a9fe056e56514edcce76846ef011a6731cc
gauravmishra8595 <gauravmishrag999@gmail.com>
2026-06-02 18:35:24 +0530

    solved sortColors

---
+++ Arrays/75_SortColors.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.size() - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            }
+            else if (nums[mid] == 1) {
+                mid++;
+            }
+            else { // nums[mid] == 2
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+    }
+};
+
+int main() {
+    vector<int> nums = {2, 0, 2, 1, 1, 0};
+
+    Solution obj;
+    obj.sortColors(nums);
+
+    cout << "Sorted array: ";
+    for (int x : nums) {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
