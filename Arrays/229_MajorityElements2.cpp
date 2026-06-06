464321c80df8855cd04d1d0f8e2f7badc74e23fa
gauravmishra8595 <gauravmishrag999@gmail.com>
2026-06-06 16:38:51 +0530

    Solved majorityelements2

---
+++ Arrays/229_MajorityElements2.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> majorityElement(vector<int> &nums)
+    {
+        int n = nums.size();
+
+        int candidate1 = 0, candidate2 = 0;
+        int count1 = 0, count2 = 0;
+
+        // 1st pass: find candidates
+        for (int num : nums)
+        {
+            if (num == candidate1)
+            {
+                count1++;
+            }
+            else if (num == candidate2)
+            {
+                count2++;
+            }
+            else if (count1 == 0)
+            {
+                candidate1 = num;
+                count1 = 1;
+            }
+            else if (count2 == 0)
+            {
+                candidate2 = num;
+                count2 = 1;
+            }
+            else
+            {
+                count1--;
+                count2--;
+            }
+        }
+
+        // 2nd pass: verify
+        count1 = 0;
+        count2 = 0;
+
+        for (int num : nums)
+        {
+            if (num == candidate1)
+            {
+                count1++;
+            }
+            else if (num == candidate2)
+            {
+                count2++;
+            }
+        }
+
+        vector<int> ans;
+
+        if (count1 > n / 3)
+            ans.push_back(candidate1);
+
+        if (count2 > n / 3)
+            ans.push_back(candidate2);
+
+        return ans;
+    }
+};
+
+int main()
+{
+    Solution obj;
+
+    vector<int> nums = {3, 2, 3};
+
+    vector<int> result = obj.majorityElement(nums);
+
+    cout << "Majority elements (> n/3): ";
+    for (int x : result)
+    {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
